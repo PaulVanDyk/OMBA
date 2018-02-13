d4d5d705c15c3c9b7358e8589e78d962d29d8483
atompi <atompi@foxmail.com>
2018-02-13 16:47:19 +0800

    Update ansible api v2

--- OMBA/utils/ansible_api_v2.py
+++ OMBA/utils/ansible_api_v2.py
@@ -282,8 +282,16 @@ class PlayBookResultsCollectorToSave(CallbackBase):
         if self.logId: AnsibleSaveResult.PlayBook.insert(self.logId, msg)
 
 class PlayBookResultsCollector(CallbackBase):
+    """
+    PlayBookResultsCollector
+    """
     CALLBACK_VERSION = 2.0
     def __init__(self, *args, **kwargs):
+        """
+
+        :param args:
+        :param kwargs:
+        """
         super(PlayBookResultsCollector, self).__init__(*args, **kwargs)
         self.task_ok = {}
         self.task_skipped = {}
@@ -306,3 +314,117 @@ class PlayBookResultsCollector(CallbackBase):
 
     def v2_runner_on_changed(self, result):
         self.task_changed[result._host.get_name()] = result
+
+    def v2_playbook_on_stats(self, stats):
+        hosts = sorted(stats.processed.keys())
+        for h in hosts:
+            t = stats.summarize(h)
+            self.task_status[h] = {
+                "ok": t['ok'],
+                "changed": t['changed'],
+                "unreachable": t['unreachable'],
+                "skipped": t['skipped'],
+                "failed": t['failures']
+            }
+
+class ANSRunner(object):
+    """
+    This is a General object for parallel execute modules.
+    """
+    def __init__(self, resource, redisKey=None, logId=None, *args, **kwargs):
+        """
+
+        :param resource:
+        :param redisKey:
+        :param logId:
+        :param args:
+        :param kwargs:
+        """
+        self.resource = resource
+        self.inventory = None
+        self.variable_manager = None
+        self.loader = None
+        self.options = None
+        self.passwords = None
+        self.callback = None
+        self.__initializeData(kwargs)
+        self.results_raw = {}
+        self.redisKey = redisKey
+        self.logId = logId
+    def __initializeData(self, kwargs):
+        """
+        初始化 ansible
+        :param kwargs:
+        :return:
+        """
+        Options = namedtuple(
+            'Options',
+            ['connection', 'module_path', 'forks', 'timeout', 'remote_user',
+             'ask_pass', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args',
+             'scp_extra_args', 'become', 'become_method', 'become_user', 'ask_value_pass',
+             'verbosity', 'check', 'listhosts', 'listtasks', 'listtags', 'syntax']
+        )
+        self.variable_manager = VariableManager()
+        self.loader = DataLoader()
+        self.options = Options(
+            connection='smart',
+            module_path=None,
+            forks=100,
+            timeout=10,
+            remote_user=kwargs.get('remote_user','root'),
+            ask_pass=False,
+            private_key_file=None,
+            ssh_common_args=None,
+            ssh_extra_args=None,
+            sftp_extra_args=None,
+            scp_extra_args=None,
+            become=True,
+            become_method=kwargs.get('become_method','sudo'),
+            become_user=kwargs.get('become_user','root'),
+            verbosity=kwargs.get('verbosity',None),
+            check=False,
+            listhosts=False,
+            listtasks=False,
+            listtags=False,
+            syntax=False,
+            ask_value_pass=False,
+        )
+        self.passwords = dict(sshpass=None, becomepass=None)
+        self.inventory = MyInventory(self.resource, self.loader, self.variable_manager).inventory
+        self.variable_manager.set_inventory(self.inventory)
+
+    def run_model(self, host_list, module_name, module_args):
+        """
+        run module from ansible ad-hoc.
+        :param host_list:
+        :param module_name: ansible module name
+        :param module_args: ansible module args
+        :return:
+        """
+        play_source = dict(
+            name="Ansible Play",
+            hosts=host_list,
+            gather_facts='no',
+            tasks=[dict(action=dict(module=module_name, args=module_args))]
+        )
+        play = Play().load(play_source, variable_manager=self.variable_manager, loader=self.loader)
+        tqm = None
+        if self.redisKey or self.logId:self.callback = ModelResultsCollectorToSave(self.redisKey, self.logId)
+        else:self.callback = ModelResultsCollector()
+        try:
+            tqm = TaskQueueManager(
+                inventory=self.inventory,
+                variable_manager=self.variable_manager,
+                loader=self.loader,
+                options=self.options,
+                passwords=self.passwords,
+            )
+            tqm._stdout_callback = self.callback
+            constants.HOST_KEY_CHECKING = False
+            tqm.run(play)
+        except Exception as err:
+            if self.redisKey:DsRedis.OpsAnsibleModel.lpush(self.redisKey, data=err)
+            if self.logId:AnsibleSaveResult.Model.insert(self.logId, err)
+        finally:
+            if tqm is not None:
+                tqm.cleanup()
