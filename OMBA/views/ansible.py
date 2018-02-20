cca153985c97de2c59e2384ad6e46d9d463d368d
atompi <atompi@foxmail.com>
2018-02-20 20:18:20 +0800

    Update ansible views

--- OMBA/views/ansible.py
+++ OMBA/views/ansible.py
@@ -129,7 +129,11 @@ def apps_model(request):
                 DsRedis.OpsAnsibleModel.delete(redisKey)
                 DsRedis.OpsAnsibleModel.lpush(
                     redisKey,
-                    "[Start] Ansible Model: {model}  ARGS:{args}".format(model=model_name, args=request.POST.get('ansible_args', "None")))
+                    "[Start] Ansible Model: {model}  ARGS:{args}".format(
+                        model=model_name,
+                        args=request.POST.get('ansible_args', "None")
+                    )
+                )
                 if request.POST.get('ansible_debug') == 'on':
                     ANS = ANSRunner(resource, redisKey, logId, verbosity=4)
                 else:
@@ -140,8 +144,122 @@ def apps_model(request):
                     module_args=request.POST.get('ansible_args', "")
                 )
                 DsRedis.OpsAnsibleModel.lpush(redisKey, "[Done] Ansible Done.")
-                return JsonResponse({'msg': "操作成功", "code": 200, 'data': []})
+                return JsonResponse(
+                    {
+                        'msg': "操作成功",
+                        "code": 200,
+                        'data': []
+                    }
+                )
             else:
                 return JsonResponse({'msg': "操作失败，未选择主机或者该分组没有成员", "code": 500, 'data': []})
         else:
             return JsonResponse({'msg': "操作失败，不支持的操作类型", "code": 500, 'data': []})
+
+@login_required()
+def ansible_run(request):
+    if request.method == "POST":
+        redisKey = request.POST.get('ans_uuid')
+        msg = DsRedis.OpsAnsibleModel.rpop(redisKey)
+        if msg:
+            return JsonResponse(
+                {
+                    'msg': msg,
+                    'code': 200,
+                    'data': []
+                }
+            )
+        else:
+            return JsonResponse(
+                {
+                    'msg': None,
+                    'code': 200,
+                    'data': []
+                }
+            )
+
+@login_required()
+@permission_required('OMBA.can_add_ansible_playbook', login_url='/noperm/')
+def app_upload(request):
+    if request.method == "GET":
+        serverList = Server_Assets.objects.all()
+        projectList = Project_Assets.objects.all()
+        groupList = Group.objects.all()
+        userList = User.objects.all()
+        serviceList = Service_Assets.objects.all()
+        return render(
+            request,
+            'app/apps_playbook_upload.html',
+            {
+                "user": request.user,
+                "userList": userList,
+                "serverList": serverList,
+                "groupList": groupList,
+                "serviceList": serviceList,
+                "projectList": projectList
+            },
+        )
+    elif request.method == "POST":
+        sList = []
+        if request.POST.get('server_model') in ['service', 'group', 'custom']:
+            if request.POST.get('server_model') == 'custom':
+                for sid in request.POST.getlist('playbook_server'):
+                    server = Service_Assets.objects.get(id=sid)
+                    sList.append(server.ip)
+                playbook_server_value = None
+            elif request.POST.get('server_model') == 'group':
+                serverList = Assets.objects.filter(group=request.POST.get('ansible_group'))
+                sList = [s.server_assets.ip for s in serverList]
+                playbook_server_value = request.POST.get('ansible_group')
+            elif request.POST.get('server_model') == 'service':
+                serverList = Assets.objects.filter(business=request.POST.get('ansible_service'))
+                sList = [s.server_assets.ip for s in serverList]
+                playbook_server_value = request.POST.get('ansible_service')
+        try:
+            playbook = Ansible_Playbook.objects.create(
+                playbook_name=request.POST.get('playbook_name'),
+                playbook_desc=request.POST.get('playbook_desc'),
+                playbook_vars=request.POST.get('playbook_vars'),
+                playbook_uuid=uuid.uuid4(),
+                playbook_file=request.FILES.get('playbook_file'),
+                playbook_server_model=request.POST.get('server_model', 'custom'),
+                playbook_server_value=playbook_server_value,
+                playbook_auth_group=request.POST.get('playbook_auth_group', 0),
+                playbook_auth_user=request.POST.get('playbook_auth_user', 0),
+                playbook_type=0,
+            )
+        except Exception, e:
+            return render(
+                request,
+                'apps/apps_playbook_upload.html',
+                {
+                    "user": request.user,
+                    "errorInfo": "剧本添加错误：%s" % str(e)
+                },
+            )
+        for sip in sList:
+            try:
+                Ansible_Playbook_Number.objects.create(
+                    playbook=playbook,
+                    playbook_server=sip
+                )
+            except Exception, e:
+                playbook.delete()
+                return render(
+                    request,
+                    'apps/apps_playbook_upload.html',
+                    {
+                        "user": request.user,
+                        "errorInfo": "目标服务器信息添加错误：%s" % str(e)
+                    },
+                )
+        # 操作日志异步记录
+        AnsibleRecord.PlayBook.insert(
+            user=str(request.user),
+            ans_id=playbook.id,
+            ans_name=playbook.playbook_name,
+            ans_content="添加Ansible剧本",
+            ans_server=','.join(sList)
+        )
+        return HttpResponseRedirect('apps/playbook/upload/')
+    
